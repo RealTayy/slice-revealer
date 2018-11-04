c64274bf9e92e0f8f7ff15e15b9de310dda3ec9d
TheMaiEQX <mai.the.09@gmail.com>
2018-11-03 19:38:04 -0500

    add initializeSlices

--- src/javascript/demo1.js
+++ src/javascript/demo1.js
@@ -1,14 +1,8 @@
 // DELETEME
 console.log('demo1.js Loaded');
 
-window.onload = function () {
-	var elements = document.getElementsByClassName('test-boxes-one');	
-	var slicer = sliceRevealer(elements);
-	slicer.doSomething();		
+window.onload = function () {	
 	var element = document.getElementById('box-two')
-	var slicerTwo = sliceRevealer(element, {kappakappa: 999});
-	slicerTwo.doSomething()
-	 
+	var slicerTwo = sliceRevealer(element, {kappakappa: 'Custom Options'});
+	slicerTwo.doSomething()	 
 }
-
-// style="height: 360px; width: 288px; top: 1160px; left: 648px;"
--- src/javascript/sliceRevealer.js
+++ src/javascript/sliceRevealer.js
@@ -20,38 +20,80 @@ function sliceRevealer(target, options) {
 		if (e instanceof TypeError) {
 			throw new TypeError(`Expected an DOM Element but instead got a ${typeof target}`);
 		} else {
-			throw new Error('Error when creating SliceRevealer Instance');
+			throw new Error(e);
 		}
 	}
 }
 
 function SliceRevealer(target, options) {
 	// Initialize Instance of SliceRevealer
-
 	var defaultOptions = {
 		kappa: 123,
 		kappakappa: 456
 	};
-
-	this.target = target;
+	
 	this.options = { ...defaultOptions, ...options };
+	this.target = target;	
 	this.container = initializeSRContainer(target);
+	this.slices = initializeSRSlices(this.options, this.container);
 	this.doSomething = function () {
 		console.log(this);
 	}
 }
 
 function initializeSRContainer(target) {
-	var sr__container = document.createElement("div");
-	sr__container.className = "sr__container";
+	var sr__container = document.createElement('div');
+	sr__container.className = 'sr__container';
 
 	// Set sr__container height and width to be same as target	
-	sr__container.style.height = target.getBoundingClientRect().height + 1 + "px";
-	sr__container.style.width = target.getBoundingClientRect().width + 1 + "px";
+	sr__container.style.height = '100%';
+	sr__container.style.width = '100%';
 
-	// set sr__container to position over target
-	sr__container.style.top = target.offsetTop + "px";
-	sr__container.style.left = target.offsetLeft + "px";
+	// Append sr__container to target Element
+	target.appendChild(sr__container);	
+	
+	return sr__container;
+}
 
-	document.body.appendChild(sr__container);
+function initializeSRSlices(options, container) {
+	const slices = 5;
+	const style = 'vertical';
+	const transitionDuration = '.5s';
+	const startingPosition = 'right'
+	for (var i = 0; i < slices; i ++) {
+		var sr__slice = document.createElement('div');
+		sr__slice.className = 'sr__slice';
+		
+		// Set Duration
+		sr__slice.style.transitionDuration = transitionDuration;
+		
+		// Set Starting Position
+		switch(startingPosition) {
+			case 'top'		: this.addClass(sr__slice, 'sr__top'); break;
+			case 'bottom'	: this.addClass(sr__slice, 'sr__bottom'); break;
+			case 'left'		: this.addClass(sr__slice, 'sr__left'); break;
+			case 'right'	: this.addClass(sr__slice, 'sr__right'); break;
+			case 'middle'	: this.addClass(sr__slice, 'sr__middle'); break;
+			default: this.addClass(sr__slice, 'sr__left');
+		}
+		
+		// Create slice 
+		if (style === 'vertical') {		
+			// Create Vertical slice
+			sr__slice.style.height = '100%';
+			sr__slice.style.width = (100/slices) + '%';
+		} 	
+		else {
+			// Create Horizontal slice
+			sr__slice.style.height = (100/slices) + '%';
+			sr__slice.style.width = '100%';			
+		}
+		container.appendChild(sr__slice);		
+		console.log(sr__slice);		
+	}
+}
+
+// Made simple function to add Class because IE doesn't support 
+function addClass(element, classes) {	
+	element.className = element.className + ' ' + classes;
 }
