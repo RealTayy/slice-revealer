6c1498f410920d2e170bebb6ce5bf3a6b0d94281
The Mai <Mai.The.09@gmail.com>
2018-11-06 22:55:26 -0600

    add LICENSE.md. edit  README.md.

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-# slice-revealer
+# Slice Revealer
+Slice Revealer is a easy to use dependancy free library that allows users to cover and uncover images with customizable animated slices. The idea was inspired by [Zhenya Rynzhuk](https://zhenyary.com/) of [Sochnik](https://dribbble.com/Sochnik)'s [transition experiments](https://dribbble.com/shots/4132057-Selected-Works-Transitions-Experiments).
 
-Noters for readme:
-> Can take jQuery object or Native DOM element.
-> Only selects first one found
-> ONE PIXEL GOOGLE CHROME .99999
-> destroy on completion
+## Credits
+
+## License
+
+## Shameless Plugs
+Interested in who made this? Come visit my portfolio at [MaiCoding](http://www.maiCoding.me)
+Want more sick and easy to use animations? Come over to [AnimationZone](http://www.maiCoding.me)
--- src/javascript/test.js
+++ src/javascript/test.js
@@ -1,12 +1,24 @@
-function Constructor(options) {
-	var defaultOptions = {
-		one: 'a',
-		two: 'b',
-		three: 'c',
-		...options
-	}
-}
+window.onload = function () {
+	var element = document.getElementsByClassName('box-two')[0];
 
-var cons = new Constructor();
-var options = {};
-console.log(options);
+	var options = {
+		direction: 'horizontal',
+		numOfSlices: 8,
+		// sliceDuration: 1,
+		// totalDuration: 1.5,
+		startPosition: 'left',
+		endPosition: 'middle',
+		startOpacity: .999999, // startOpacity set to .999 because chrome doesn't play well with rounding pixels and sometimes is one pixel off.
+		endOpacity: 1,
+	}
+
+	var slicerTwo = sliceRevealer(element, options);
+
+	element.addEventListener('mouseenter', function () {
+		slicerTwo.doIt()
+	})
+
+	element.addEventListener('mouseleave', function () {
+		slicerTwo.undoIt();		
+	})
+}
