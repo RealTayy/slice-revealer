25f0e38e17f7cf54454fff9b67ad3e61cd332ae4
The Mai <Mai.The.09@gmail.com>
2018-11-27 01:57:47 -0600

    add colors and styles to nav boxes

--- demo/demo2/demo2.js
+++ demo/demo2/demo2.js
@@ -17,26 +17,51 @@ document.addEventListener("DOMContentLoaded", function (event) {
 		// Jan
 		{
 			...defaultNavOptions,
+			color: "#BB552D"
 		},
 		// Feb
 		{
 			...defaultNavOptions,
+			color: "#FF5056",
+			direction: "vertical",
+			endPosition: ["top", "bottom"],
+			numOfSlices: 2,
+			sliceDuration: 1.3,
+
 		},
 		// Mar
 		{
 			...defaultNavOptions,
+			color: "#39561E",
+			direction: "vertical",
+			endPosition: "top",
+			numOfSlices: 4,
+			transitionOrder: "reverse",
 		},
 		// Apr
 		{
 			...defaultNavOptions,
+			color: "#365874",
+			endPosition: ["left", "right", "left", "right", "left", "right"],
+			numOfSlices: 6,
 		},
 		// May
 		{
 			...defaultNavOptions,
+			color: "#36415A",
+			direction: "vertical",
+			endPosition: "bottom",
+			numOfSlices: 13,
+			transitionOrder: [6, 7, 5, 8, 4, 9, 3, 10, 2, 11, 1, 12, 0],
 		},
 		// Jun
 		{
 			...defaultNavOptions,
+			color: "#EEA25F",
+			endPosition: ["left", "left", "left", "left", "left", "left", "right", "right", "right", "right", "right", "right"],
+			numOfSlices: 12,
+			transitionOrder: [0, 11, 1, 10, 2, 9, 3, 8, 4, 7, 5, 6]
+
 		},
 		// Jul
 		{
